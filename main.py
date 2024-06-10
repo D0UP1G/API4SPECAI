957a0a5e61742d5f9dced52da82134a78cfaea7c
DO U PIG <113414349+D0UP1G@users.noreply.github.com>
2024-06-10 19:48:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from urllib.parse import urlencode
 while True:
-    a = input('[1]Получить Токен\n[2]Получить о товаре\nПолучить: ')
+    a = input('[1]Получить Токен\n[2]Получить о товаре \nПолучить: ')
     try:
         if int(a) == 1:
             print(requests.post('https://d5dhfr3ddkpc5v7acc8h.apigw.yandexcloud.net/token').text)
@@ -12,7 +12,7 @@ while True:
                 'brand':input('Brand: ')
             }
 
-            print(requests.get(f'https://d5dhfr3ddkpc5v7acc8h.apigw.yandexcloud.net/get_sign?{urlencode(data)}').text)
+            print(requests.get(f'https://d5dhfr3ddkpc5v7acc8h.apigw.yandexcloud.net/get_description?{urlencode(data)}').text)
             
     except:
         print('Ты видимо Биттер')
